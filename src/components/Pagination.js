fac192ad6d308172fd5e5562017f2e72976f842f
Renato Pessoa <rpessoa@MacBook-Pro-de-Renato.local>
2020-07-08 23:30:35 -0300

    troca na paginação de anchor para button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,9 +12,12 @@ const Pagination = ({ itemsPerPage, totalPokemons, paginate}) => {
       <ul className='pagination'>
         {pageNumbers.map(number => (
           <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+            <button 
+              onClick={() => paginate(number)} 
+              className='page-link'
+            >
               {number}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
